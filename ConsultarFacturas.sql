a756bba32d533f53d308e8d3775746dfd4a73fcc
Alel385 <127790152+Alel385@users.noreply.github.com>
2025-11-25 22:47:45 -0600

    Refactor sp_ConsultarFacturasPortal procedure

--- ConsultarFacturas.sql
+++ ConsultarFacturas.sql
@@ -1,82 +1,60 @@
 CREATE OR ALTER PROCEDURE sp_ConsultarFacturasPortal
-    @NumeroFinca VARCHAR(50) = NULL,
-    @ValorDocumentoIdentidad VARCHAR(50) = NULL
+    @TerminoBusqueda NVARCHAR(120) = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
-    
-    DECLARE @ResultCode INT = 0;
 
-    BEGIN TRY
-        BEGIN TRANSACTION;
+    --Limpieza de parÃ¡metro
+    IF @TerminoBusqueda IS NOT NULL
+    BEGIN
+        SET @TerminoBusqueda = LTRIM(RTRIM(@TerminoBusqueda));
+        IF @TerminoBusqueda = '' SET @TerminoBusqueda = NULL;
+    END
 
-        --Validación
-        IF @NumeroFinca IS NULL AND @ValorDocumentoIdentidad IS NULL
-        BEGIN
-            PRINT 'Error: Debe proporcionar al menos un parámetro.';
-            ROLLBACK TRANSACTION;
-            RETURN -1;
-        END
+    IF @TerminoBusqueda IS NULL RETURN;
 
-        --Consulta
-        SELECT DISTINCT
-            F.Id AS IdFactura,
-            P.NumeroFinca,
-            F.NumeroFactura,
-            F.FechaEmision,
-            F.FechaVencimiento,
-            F.IdEstadoFactura, 
-            
-            CASE 
-                WHEN F.IdEstadoFactura = 1 AND GETDATE() > F.FechaVencimiento 
-                THEN DATEDIFF(DAY, F.FechaVencimiento, GETDATE())
-                ELSE 0
-            END AS DiasAtraso,
+    DECLARE @FiltroLike NVARCHAR(122) = '%' + @TerminoBusqueda + '%';
 
-            F.TotalAPagarFinal AS MontoTotal,
-            EF.Nombre AS Estado,
-            
-            CASE 
-                WHEN F.IdEstadoFactura = 1 THEN 'Pendiente de Pago'
-                WHEN F.IdEstadoFactura = 2 THEN 'Pagada el ' + ISNULL(CONVERT(VARCHAR, CP.FechaPago), 'N/A')
-                ELSE EF.Nombre
-            END AS DetalleEstado
+    --Consulta
+    SELECT DISTINCT
+        F.Id AS IdFactura,
+        P.NumeroFinca,
+        F.NumeroFactura,
+        F.FechaEmision,
+        F.FechaVencimiento,
+        F.IdEstadoFactura,
+        
+        CASE 
+            WHEN F.IdEstadoFactura = 1 AND GETDATE() > F.FechaVencimiento 
+            THEN DATEDIFF(DAY, F.FechaVencimiento, GETDATE())
+            ELSE 0
+        END AS DiasAtraso,
 
-        FROM Factura F
-        INNER JOIN EstadoFactura EF ON F.IdEstadoFactura = EF.Id
-        INNER JOIN Propiedad P ON F.IdPropiedad = P.Id
-        LEFT JOIN ComprobantePago CP ON CP.IdFactura = F.Id
-        LEFT JOIN Propietario PRO ON PRO.IdPropiedad = P.Id
-        LEFT JOIN Persona PER ON PER.Id = PRO.IdPersona
+        F.TotalAPagarFinal AS MontoTotal,
+        EF.Nombre AS Estado,
+        
+        CASE 
+            WHEN F.IdEstadoFactura = 1 THEN 'Pendiente de Pago'
+            WHEN F.IdEstadoFactura = 2 THEN 'Pagada el ' + ISNULL(CONVERT(VARCHAR, CP.FechaPago), 'N/A')
+            ELSE EF.Nombre
+        END AS DetalleEstado
 
-        WHERE 
-            (@NumeroFinca IS NOT NULL AND P.NumeroFinca = @NumeroFinca)
+    FROM Factura F
+    INNER JOIN EstadoFactura EF ON F.IdEstadoFactura = EF.Id
+    INNER JOIN Propiedad P ON F.IdPropiedad = P.Id
+    LEFT JOIN ComprobantePago CP ON CP.IdFactura = F.Id
+    LEFT JOIN Propietario PRO ON PRO.IdPropiedad = P.Id
+    LEFT JOIN Persona PER ON PER.Id = PRO.IdPersona
+
+    WHERE 
+        (
+            P.NumeroFinca LIKE @FiltroLike
             OR
-            (@ValorDocumentoIdentidad IS NOT NULL AND PER.ValorDocumentoIdentidad = @ValorDocumentoIdentidad)
+            PER.ValorDocumentoIdentidad LIKE @FiltroLike
+        )
 
-        ORDER BY 
-            F.IdEstadoFactura ASC, 
-            F.FechaVencimiento ASC;
-
-        --Verificación
-        IF @@ROWCOUNT = 0
-        BEGIN
-            SET @ResultCode = 50001;--No encontrado
-            PRINT 'Aviso: No se encontraron registros.';
-        END
-        ELSE
-        BEGIN
-            SET @ResultCode = 0; -- Éxito
-        END
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
-        PRINT 'Error Interno: ' + ERROR_MESSAGE();
-        RETURN -500;
-    END CATCH
-
-    RETURN @ResultCode;
+    ORDER BY 
+        F.IdEstadoFactura ASC, 
+        F.FechaVencimiento ASC;
 END;
 GO
